1490ec0adf26fae0eae95b4dc9c502dcfad9b42e
Kevin Deras <kevin.deras@ymail.com>
2021-07-30 17:10:49 -0400

    Add files via upload
    created a music api on my free time.

---
+++ MusicAPI/API Tutorial/API_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_TutorialApp.swift
+//  API Tutorial
+//
+//  Created by Kevin Deras on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct API_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
